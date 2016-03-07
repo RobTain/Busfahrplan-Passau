3dd1a1d6009ba94f83dcf1402bf832f030bd233e
Robert <koenigro@fim.uni-passau.de>
2016-03-07 20:44:32 +0100

    Line 8/9 renaming

--- Busfahrplan-Passau/app/src/main/java/com/robtain/busfahrplan_passau/ShowEntryActivity.java
+++ Busfahrplan-Passau/app/src/main/java/com/robtain/busfahrplan_passau/ShowEntryActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -94,6 +95,7 @@ public class ShowEntryActivity extends AppCompatActivity {
 
     private void findPicture() {
         String path = "p" + keyword.toLowerCase() + "_" + busStop.getId();
+        Log.e(path, path);
 
        Resources res = getResources();
         int id = res.getIdentifier(path, "drawable",getPackageName());
